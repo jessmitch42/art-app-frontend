770c0095fa55af4b5394450cbd845d97154d71d2
jessmitch42 <jessicamitch42@gmail.com>
2018-03-29 13:00:46 -0400

    starting reducers for artists and artworks

---
+++ src/reducers/artists_reducer.js
@@ -0,0 +1,12 @@
+export default function manageArtists(state = {artists: []}, action) {
+  switch (action.type) {
+
+    case 'ADD_ARTIST':
+
+      return state
+
+    default:
+      return state;
+
+  }
+};
---
+++ src/reducers/artworks_reducer.js
@@ -0,0 +1,12 @@
+export default function manageArtworks(state = {artworks: []}, action) {
+  switch (action.type) {
+
+    case 'ADD_ARTWORK':
+
+      return state
+
+    default:
+      return state;
+
+  }
+};
