48922b3f67b4c734aeb981f9fdd1cb46949a60dc
jessmitch42 <jessicamitch42@gmail.com>
2018-03-30 17:36:57 -0400

    links for artist images added

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Artist = (props) => {
   console.log("ARTISTS HEREEE")
   console.log(props)
   return (
-    <div id={"artist-"+props.artist.id} className="artist__container">
-      <img className="artist-img" src={`http://localhost:4000/img/${props.artist.img}.png`} alt="I'm a picture"/>
+    <div id={`artist-${props.artist.id}`} className="artist__container">
+      <Link to={`/artists/${props.artist.id}`}><img className="artist-img" src={`http://localhost:4000/img/${props.artist.img}.png`} alt="Artist Artwork Example"/></Link>
       <div className="artist__name">{props.artist.first_name} {props.artist.last_name}</div>
     </div>
   );
