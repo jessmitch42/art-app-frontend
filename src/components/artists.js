2c1ef35aa16c9b92306c953292d16940c98b59ad
jessmitch42 <jessicamitch42@gmail.com>
2018-03-29 21:12:34 -0400

    ternary instead of if/else

--- src/components/artists.js
+++ src/components/artists.js
@@ -3,14 +3,7 @@ import Artist from './artist'
 
 const Artists = (props) => {
   console.log(props.artists.loading)
-  let artists;
-  if (!props.artists.loading) {
-    console.log("HEREEEKLJSFKLJSF")
-    artists = props.artists.map(a => <Artist artist={a}/>)
-  }
-  else {
-    artists = ""
-  }
+  let artists = !props.artists.loading ? props.artists.map(a => <Artist artist={a}/>) : "";
   console.log(artists)
   return (
     <div className="artists-container">
