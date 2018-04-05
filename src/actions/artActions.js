2f618401db6125c7f7634a7f3cef4b39b383ec80
jessmitch42 <jessicamitch42@gmail.com>
2018-04-05 19:43:24 -0400

    updated store to include redux devtools

--- src/actions/artActions.js
+++ src/actions/artActions.js
@@ -1,20 +1,22 @@
 export function fetchArtists() {
   console.log("in fetchArtists action")
   return (dispatch) => {
-    dispatch({ type: 'LOADING_ARTISTS' });
+    dispatch({ type: 'LOADING_DATA' });
     return fetch('http://localhost:4000/artists')
       .then(response => response.json())
-      .then(res => dispatch({ type: 'ADD_ARTISTS', artists: res }));
+      .then(res => dispatch({ type: 'ADD_ARTISTS', artists: res }))
+      .then(res => dispatch({ type: 'STOP_LOADING_DATA'}))
   };
 }
 
 export function fetchArtworks(artistId) {
   console.log("in fetchArtworks action")
+  console.log(artistId)
   return (dispatch) => {
     dispatch({ type: 'LOADING_DATA' });
     return fetch(`http://localhost:4000/artists/${artistId}/artworks`)
-      .then(response => response.json())
-      .then(res => console.log(res));
+    .then(res => dispatch({ type: 'ADD_ARTWORKS', artworks: res }))
+    .then(res => dispatch({ type: 'STOP_LOADING_DATA'}))
   };
 }
 // dispatch({ type: 'ADD_ARTWORKS', artworks: res })
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { ConnectedApp } from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 console.log(store);
 
