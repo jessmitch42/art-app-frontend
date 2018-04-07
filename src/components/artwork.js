946b78218149bc8bacf986911dcc2b37b993d317
jessmitch42 <jessicamitch42@gmail.com>
2018-04-07 15:36:41 -0400

    sorting working. needs clean up

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -5,7 +5,7 @@ class Artwork extends Component {
   render() {
     return (
       <div id={`artwork-${this.props.artwork.id}`} className="artwork__container avoid-break">
-        <img className="artwork-img" src={`http://localhost:4000/img/${this.props.artwork.img}.png`} alt={`${this.props.artwork.name} image`}/>
+        <img className="artwork-img" src={`http://localhost:4000/img/${this.props.artwork.img}.png`} alt={`${this.props.artwork.name}`}/>
 
         <div className="artwork__name">{`${this.props.artwork.name}  |  ${this.props.artwork.year_completed}`}</div>
         <div className="artist__materials">{this.props.artwork.materials}</div>
--- src/views/allArtistsContainer.js
+++ src/views/allArtistsContainer.js
@@ -1,32 +1,73 @@
 import React, { Component } from 'react';
 import ArtistPreview from '../components/artistPreview';
 // import ArtistSelect from '../components/artistSelect';
-
 import FlipMove from 'react-flip-move';
+var shuffle = require('shuffle-array');
 
 class AllArtistsContainer extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: ''
-  //   };
-  //   this.handleSelection = this.handleSelection.bind(this);
-  // }
-  //
-  // handleSelection = () => {
-  //   console.log("handleselection")
-  //   if (this.state.value) {
-  //     console.log(this.state.value)
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      sortedArtists: []
+    };
+    this.handleSelection = this.handleSelection.bind(this);
+  }
+
+  handleSelection = (e) => {
+    this.setState({ value: e.target.value });
+    let artists;
+
+    if (this.state.value === "ascending") {
+      console.log("ascendign")
+      let artists = this.sortAscending(this.props.artists);
+      console.log(artists)
+
+      this.setState({sortedArtists: artists});
+      // this.props.artists = this.sortAscending(this.props.artists);
+    }
+    else if (this.state.value === "descending") {
+      let artists = this.sortAge(this.props.artists);
+      console.log(artists)
+
+      this.setState({sortedArtists: artists});
+    }
+    else if (this.state.value === "random") {
+      let artists = this.sortRandom(this.props.artists);
+      console.log(artists)
+      this.setState({sortedArtists: artists});
+    }
+  }
+
+  sortAscending = a => a.slice().sort((a,b) => {
+    var nameA = a.last_name.toUpperCase();
+    var nameB = b.last_name.toUpperCase();
+    if (nameA < nameB) { return -1; }
+    else if (nameA > nameB) { return 1; }
+    else return 0;
+  });
+
+  sortAge = a => a.slice().sort((a,b) => b.birth_year - a.birth_year);
+
+  sortRandom = a => shuffle(a.slice());
+
 
   render() {
-    console.log(this.props)
+    const { artists } = this.props;
+    let artistComponents;
+    if (this.state.sortedArtists.length) {
+      artistComponents = this.state.sortedArtists.map(a => <ArtistPreview key={a.id} artist={a} actions={this.props.actions}/>)
+    }
+    else {
+      artistComponents = artists.map(a => <ArtistPreview key={a.id} artist={a} actions={this.props.actions}/>)
+    }
+
+
     return (
       <div>
         <div className="artists-container wrapper">
 
-        <select className="artist-select">
+        <select className="artist-select" onChange={this.handleSelection}>
           <option selected>Sort Artists</option>
           <option value="ascending">A-Z</option>
           <option value="descending">Z-A</option>
@@ -35,7 +76,7 @@ class AllArtistsContainer extends Component {
 
           <FlipMove
             staggerDelayBy={130}>
-            {this.props.artists.map(a => <ArtistPreview key={a.id} artist={a} actions={this.props.actions}/>)}
+            {artistComponents}
           </FlipMove>
         </div>
       </div>
