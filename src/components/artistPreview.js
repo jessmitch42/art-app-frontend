7e85351b85cb007efb9676e73abd43db32198390
jessmitch42 <jessicamitch42@gmail.com>
2018-04-07 16:44:56 -0400

    destructuring added for artist preview component

--- src/components/artistPreview.js
+++ src/components/artistPreview.js
@@ -5,14 +5,16 @@ import * as actions from '../actions/actions';
 class ArtistPreview extends Component {
   // this is a react component instead of a stateless functional component because the plugin "flip-move" that does the artists_container animation requires a react components as children
   render() {
-    return (
-      <div id={`artist-${this.props.artist.id}`} className="artist__container avoid-break">
-        <Link
-          to={`/artists/${this.props.artist.id}/artworks`}
-          onClick={() => this.props.actions.setCurrentArtist(this.props.artist.id)}
-        ><img className="artist-img" src={`http://localhost:4000/img/${this.props.artist.img}.png`} alt="Artist Artwork Example"/></Link>
+    const { id, img, first_name, last_name } = this.props.artist;
 
-        <div className="artist__name">{this.props.artist.first_name.toUpperCase()} {this.props.artist.last_name.toUpperCase()}</div>
+    return (
+      <div id={`artist-${id}`} className="artist__container avoid-break">
+        <Link
+          to={`/artists/${id}/artworks`}
+          onClick={() => this.props.actions.setCurrentArtist(id)}
+        ><img className="artist-img" src={`http://localhost:4000/img/${img}.png`} alt="Artist Artwork Example"/></Link>
+
+        <div className="artist__name">{first_name.toUpperCase()} {last_name.toUpperCase()}</div>
       </div>
     );
   }
