6b2e02652f8f0268838ca88c4c5a43e27c1d4d4a
jessmitch42 <jessicamitch42@gmail.com>
2018-04-12 11:25:14 -0400

    changed order of imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { ConnectedApp } from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
+import { ConnectedApp } from './App';
+import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(
